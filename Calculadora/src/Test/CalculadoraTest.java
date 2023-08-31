cc8c2bda6f5ea628f9a1ea4200bdb4d9e613744a
Hugo Macedo <hm.carvalho2002@gmail.com>
2023-08-31 17:03:40 -0300

    feat: :fire: Tudo completo com testes passando

--- Calculadora/src/Test/CalculadoraTest.java
+++ Calculadora/src/Test/CalculadoraTest.java
@@ -1,13 +1,32 @@
 package Test;
 
+import Main.Calculadora;
 import org.junit.Test;
 import static org.junit.Assert.*;
 public class CalculadoraTest {
 
     @Test
-    public void testSoma() {Calculadora calc = new Calculadora();
-        assertEquals(5, calc.soma(2, 3));
+    public void testSoma() {
+        Calculadora calculadora = new Calculadora();
+        assertEquals(5, calculadora.adicionar(2, 3),0.0001 );
         }
+
+
+    @Test
+    public void testSubtra() {
+        Calculadora calculadora = new Calculadora();
+        assertEquals(1, calculadora.subtrair(3, 2),0.0001 );
     }
+    @Test
+    public void testDiv() {
+        Calculadora calculadora = new Calculadora();
+        assertEquals(5, calculadora.dividir(10, 2),0.0001 );
+    }
+    @Test
+    public void testMulti() {
+        Calculadora calculadora = new Calculadora();
+        assertEquals(15, calculadora.multiplicar(5, 3),0.0001 );
+    }
+}
 
 
